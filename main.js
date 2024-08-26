fbb7dcd541aa6951ce67632a9bf9f4a0c9eabab2
wellington <dev.wellington.oliveira@outlook.com>
2024-08-25 23:58:06 -0300

    Alterações no modelo dos objetos

--- main.js
+++ main.js
@@ -1,55 +1,79 @@
-function Carro(marca, modelo, ano, preco, nomeDono){
-    this.marca = marca
-    this.modelo = modelo
-    this.ano = ano
-    this.preco = preco
-    this.acelerar = function(){
-        console.log(this.modelo + ' está acelerando')
+class Carro {
+    constructor(modelo, marca, ano, motor){
+        this.modelo = modelo;
+        this.marca = marca;
+        this.ano = ano;
+        this.motor = motor;
     }
-    Pessoa.call(this, nomeDono)
+
+    acelerar(){
+        console.log(`O ${this.modelo} esta acelerando!`)
+    }
+}
+
+class Moto extends Carro{
+    constructor(modelo, marca, ano, cc, dirtBike){
+        super(modelo, marca, ano)
+        this.cc = cc
+        this.dirtBike = dirtBike
+    }
+
+    dirtRoad(){
+        if(this.dirtBike == true){
+            console.log(`A moto de modelo ${this.modelo} é propria para este terreno.`)
+        }
+        else{
+            console.log(`A moto de modelo ${this.modelo} não é indicada para este terreno!`)
+        }
+    }
+}
+
+class Caminhonete extends Carro{
+    constructor(modelo, marca, ano, motor, pesoMaximo){
+        super(modelo, marca, ano, motor)
+        this.pesoMaximo = pesoMaximo;
+    }
+
+    transporteCarga(){
+        console.log(`Esta caminhonete de modelo ${this.modelo} transporta até ${this.pesoMaximo}`)
+    }
 }
 
 //instâncias de Carro
-const Civic = new Carro('honda', 'civic', '2020', '80.000')
-const Fusca = new Carro('volkswagen', 'fusca', '1959', '10.000')
-const Opala = new Carro('chevrolet', 'Opala', '1968', '20.000')
+const Fusca = new Carro('Fusca', 'volkswagen', '1960', '1500');
+const Opala = new Carro('Opala', 'chevrolet', '1968', '2.0');
+const E500 = new Carro('E500', 'mercedes', '2012', '5.5 v8');
 
-function Pessoa(nome, idade){
-    this.nome = nome;
-    this.idade = idade;
-    let _contaBanco = this.setConta = function (i){
-        return console.log(`A conta de ${this.nome} é ${i}`)
-    } 
-}
+//instâncias de Moto 
+const Ktm = new Moto('enduro', 'ktm', '2018', '150', true);
+const Titan = new Moto('Cg', 'Honda', '2020', '150', false);
+const Tornado = new Moto('Roadster', 'Honda', '2000', '250',  true);
 
-//instâncias de pessoa
-const Nicolas = new Pessoa('Nicolas', 18)
-const Wellington = new Pessoa('Wellington', 21)
-const Lucas = new Pessoa('Lucas', 28)
+//instâncias de Caminhonete
+const Pampa = new Caminhonete('Pampa', 'ford', '1992', '2.0', '440kg');
+const L200 = new Caminhonete('L200', 'mitsubishi', '2010', '2.4D', '1.050kg');
+const Ram = new Caminhonete('Ram', 'dodge', '2023', '6.7D', '1752kg');
 
-//settings das contas
-console.log(Nicolas)
-console.log(Nicolas.setConta("60.000"))
-console.log(Wellington)
-console.log(Wellington.setConta("40.000"))
-console.log(Lucas)
-console.log(Lucas.setConta("30.000"))
-
-
-//ações do civic
-Civic.nome = Wellington.nome;
-Civic.idade = Wellington.idade
-console.log(Civic)
-Civic.acelerar()
-
-//ações do Fusca
-Fusca.nome = Nicolas.nome
-Fusca.idade = Nicolas.idade
-console.log(Fusca)
+//Ações de Carro
+console.log(Fusca, Opala, E500);
 Fusca.acelerar()
-
-//ações do Opala
-Opala.nome = Lucas.nome
-Opala.idade = Lucas.idade
-console.log(Opala)
 Opala.acelerar()
+E500.acelerar()
+
+//Ações de Moto
+console.log(Ktm, Titan, Tornado);
+Ktm.dirtRoad()
+Titan.dirtRoad()
+Tornado.dirtRoad()
+Ktm.acelerar()
+Tornado.acelerar()
+Titan.acelerar()
+
+//Ações de Caminhonete
+console.log(Pampa, L200, Ram);
+Pampa.transporteCarga();
+L200.transporteCarga();
+Ram.transporteCarga();
+Pampa.acelerar();
+L200.acelerar();
+Ram.acelerar();
